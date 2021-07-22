99f073eef25fd4a41187f766193ca686ad198776
Gabriel <gabrielmdoliveira@gmail.com>
2021-07-22 15:53:55 -0300

    Movido passo do arquivo cadastro_steps.rb para o arquivo shared_steps.rb

--- rocklov/features/step_definitions/shared_steps.rb
+++ rocklov/features/step_definitions/shared_steps.rb
@@ -1,3 +1,8 @@
 Entao('sou redirecionado para o Dashboard') do
     expect(page).to have_css ".dashboard"
 end
+
+Entao('vejo a mensagem de alerta: {string}') do |expect_alert|
+    alert = find(".alert-dark")
+    expect(alert.text).to eql expect_alert
+end
