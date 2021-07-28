e606c8888545e783867b075a0b9696df2756c21f
Gabriel <gabrielmdoliveira@gmail.com>
2021-07-28 20:59:02 -0300

    Organizada a estrutura de pastas para implementação dos testes de API

--- rocklov/web/features/support/env.rb
+++ rocklov/web/features/support/env.rb
@@ -5,7 +5,7 @@ require "faker"
 
 Capybara.configure do |config|
     #config.default_driver = :selenium
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium_chrome_headless
     config.app_host = "http://rocklov-web:3000"
     config.default_max_wait_time = 10
 end
