f331e56382be3500c2b6918bb52aac36a506f795
Gabriel <gabrielmdoliveira@gmail.com>
2021-07-27 21:52:21 -0300

    Inclusão do arquivo cucumber.yml. Implementação do allure reports e hook para tirar screenshot após execução do cenário

--- rocklov/features/support/hooks.rb
+++ rocklov/features/support/hooks.rb
@@ -5,3 +5,13 @@ Before do
     @equipos_page = EquiposPage.new
     @alert = Alert.new
 end
+
+After do 
+    temp_shot = page.save_screenshot("logs/temp_screenshot.png")
+    #metodo do allure para inclusao do screenshot no report gerado
+    Allure.add_attachment(
+        name: "Screenshot",
+        type: Allure::ContentType::PNG,
+        source: File.open(temp_shot)
+    )
+end
