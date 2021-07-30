4eddafaa7d9434ef652c20b74dccfe9b2c27d213
Gabriel <gabrielmdoliveira@gmail.com>
2021-07-29 22:20:08 -0300

    Implementado metodo contexto dentro da validação de login com sucesso

--- rocklov/api/spec/post_session_spec.rb
+++ rocklov/api/spec/post_session_spec.rb
@@ -1,18 +1,24 @@
 require "httparty"
 
 describe "POST /sessions" do
-    it "login com sucesso" do
+    context "login com sucesso" do 
+        before(:all) do
+            payload = {email: "gabriel_teste@gmail.com", password: "pwd123"}
 
-        payload = {email: "gabriel_teste@gmail.com", password: "pwd123"}
+            @result = HTTParty.post("http://rocklov-api:3333/sessions",
+            body: payload.to_json,
+            headers: {
+                "Content-Type": "application/json",
+                }
+            )
+        end
+    
+        it "valida status code" do  
+            expect(@result.code).to eql 200
+        end
 
-        result = HTTParty.post("http://rocklov-api:3333/sessions",
-        body: payload.to_json,
-        headers: {
-            "Content-Type": "application/json",
-            }
-        )
-
-        expect(result.code).to eql 200
-        expect(result.parsed_response["_id"].length).to eql 24
+        it "valida id do usuario" do
+            expect(@result.parsed_response["_id"].length).to eql 24
+        end
     end
 end
