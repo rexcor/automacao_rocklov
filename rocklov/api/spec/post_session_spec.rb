a426ad24c9ac20e401ac726acb55e31eac3f1edb
Gabriel <gabrielmdoliveira@gmail.com>
2021-07-31 18:18:09 -0300

    Ajuste no código para cenários negativos

--- rocklov/api/spec/post_session_spec.rb
+++ rocklov/api/spec/post_session_spec.rb
@@ -16,18 +16,58 @@ describe "POST /sessions" do
         end
     end
 
-    context "senha invalida" do 
-        before(:all) do
-            payload = {email: "gabriel_teste@gmail.com ", password: "123456"}
-            @result = Sessions.new.login(payload)
-        end
-    
-        it "valida status code" do  
-            expect(@result.code).to eql 401
-        end
+    examples = [
+        {
+            title: "Senha invalida",
+            payload: {email: "gabriel_teste@gmail.com", password: "123456"},
+            code: 401,
+            error: "Unauthorized",
+        },
+        {
+            title: "Usuario inexistente",
+            payload: {email: "naoexiste@gmail.com", password: "pwd123"},
+            code: 401,
+            error: "Unauthorized"
+        },
+        {
+            title: "Email em branco",
+            payload: {email: "", password: "pwd123"},
+            code: 412,
+            error: "required email"
+        },
+        {
+            title: "Sem campo email",
+            payload: {password: "pwd123"},
+            code: 412,
+            error: "required email"
+        },
+        {
+            title: "Senha em branco",
+            payload: {email: "gabriel_teste@gmail.com", password: ""},
+            code: 412,
+            error: "required password"
+        },
+        {
+            title: "Sem campo senha",
+            payload: {email: "gabriel_teste@gmail.com"},
+            code: 412,
+            error: "required password"
+        },
+    ]
 
-        it "valida id do usuario" do
-            expect(@result.parsed_response["error"]).to eql "Unauthorized"
+    examples.each do |e|
+        context "#{e[:title]}" do 
+            before(:all) do
+                @result = Sessions.new.login(e[:payload])
+            end
+        
+            it "valida status #{e[:code]}" do  
+                expect(@result.code).to eql e[:code]
+            end
+    
+            it "mensagem de erro" do
+                expect(@result.parsed_response["error"]).to eql e[:error]
+            end
         end
     end
 end
