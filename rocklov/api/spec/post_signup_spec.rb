ec211df629e8274076b1b90ca367fba828b6ae8e
Gabriel <gabrielmdoliveira@gmail.com>
2021-08-01 13:49:19 -0300

    Implementado cenários de teste negativos para POST/Signup

--- rocklov/api/spec/post_signup_spec.rb
+++ rocklov/api/spec/post_signup_spec.rb
@@ -19,4 +19,86 @@ describe "POST /signup" do
         end
 
     end
-end
+
+    context "usuario ja cadastrado" do
+        before(:all) do
+            payload = {name: "Arya Stark", email: "arya@stark.com.br", password: "pwd123"}
+            MongoDB.new.remove_user(payload[:email])
+            Signup.new.create(payload)
+            @result = Signup.new.create(payload)
+        end
+
+        it "valida status code" do
+            expect(@result.code).to eql 409
+        end
+
+        it "mensagem de erro" do
+            expect(@result.parsed_response["error"]).to eql "Email already exists :("
+        end
+
+        it "valida bussiness code 1001" do
+            expect(@result.parsed_response["code"]).to eql 1001
+        end
+    end
+
+    examples = [
+        {
+            title: "Nome em branco",
+            payload: {name: "", email: "arya@stark.com.br", password: "pwd123"},
+            code: 412,
+            error: "required name",
+        },
+        {
+            title: "Sem campo nome",
+            payload: {email: "arya@stark.com.br", password: "pwd123"},
+            code: 412,
+            error: "required name",
+        },
+        {
+            title: "Email em branco",
+            payload: {name: "Arya", email: "", password: "pwd123"},
+            code: 412,
+            error: "required email",
+        },
+        {
+            title: "Sem campo email",
+            payload: {name: "Arya", password: "pwd123"},
+            code: 412,
+            error: "required email",
+        },
+        {
+            title: "Email invalido",
+            payload: {name: "Arya", email: "arya#stark.com.br", password: "pwd123"},
+            code: 412,
+            error: "wrong email",
+        },
+        {
+            title: "Senha em branco",
+            payload: {name: "Arya", email: "arya@stark.com.br", password: ""},
+            code: 412,
+            error: "required password",
+        },
+        {
+            title: "Sem campo senha",
+            payload: {name: "Arya", email: "arya@stark.com.br"},
+            code: 412,
+            error: "required password",
+        }
+    ]
+
+    examples.each do |e|
+        context "#{e[:title]}" do
+            before(:all) do
+                @result = Signup.new.create(e[:payload])
+            end
+
+            it "valida status #{e[:code]}" do
+                expect(@result.code).to eql e[:code]
+            end
+
+            it "mensagem de erro" do
+                expect(@result.parsed_response["error"]).to eql e[:error]    
+            end
+        end    
+    end
+end    
