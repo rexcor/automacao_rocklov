4c10cf38da6e1bcc07f941e739678e34fe8fcf93
Gabriel <gabrielmdoliveira@gmail.com>
2021-08-19 21:26:12 -0300

    Implementada rakefile para o modulo  web

--- rocklov/web/features/support/libs/mongo.rb
+++ rocklov/web/features/support/libs/mongo.rb
@@ -1,12 +1,20 @@
 require 'mongo'
 
 class MongoDB    
-    attr_accessor :users, :equipos
+    attr_accessor :client, :users, :equipos
     
     def initialize
-        client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
+        @client = Mongo::Client.new('mongodb://rocklov-db:27017/rocklov')
         @users = client[:users]
         @equipos = client[:equipos]
+    end
+
+    def drop_danger
+        @client.database.drop
+    end
+
+    def insert_users(docs)
+        @users.insert_many(docs)
     end
 
     def remove_user(email)
